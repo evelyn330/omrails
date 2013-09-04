e4a3b5a2256a372c166bb3447f8a89d0c2d6446a
Evelyn Chang <evelyn330@gmail.com>
2013-09-04 17:14:04 -0400

    Add User Profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Omrails::Application.routes.draw do
   
+  get "users/show"
   resources :pins
   devise_for :users
+  get 'users/:id' => 'users#show', as: :user
+
+
   get "about" => "pages#about"
   root :to => 'pins#index'
   # The priority is based upon order of creation: first created -> highest priority.
